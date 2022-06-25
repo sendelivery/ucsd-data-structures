5fb4c6328a2904d3717cbe0f74e59bcb901fc5e2
Seb <seb@192.168.1.114>
2022-06-25 16:47:48 +0100

    Week 1 - Brackets in Code

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
@@ -2,13 +2,17 @@
 #include <stack>
 #include <string>
 
-struct Bracket {
-    Bracket(char type, int position):
-        type(type),
-        position(position)
-    {}
+using namespace std;
 
-    bool Matchc(char c) {
+struct Bracket
+{
+    Bracket(char type, int position) : type(type),
+        position(position)
+    {
+    }
+
+    bool Matchc(char c)
+    {
         if (type == '[' && c == ']')
             return true;
         if (type == '{' && c == '}')
@@ -23,23 +27,46 @@ struct Bracket {
 };
 
 int main() {
-    std::string text;
-    getline(std::cin, text);
+    string text;
+    getline(cin, text);
 
-    std::stack <Bracket> opening_brackets_stack;
-    for (int position = 0; position < text.length(); ++position) {
+    stack<Bracket> opening_brackets_stack;
+    for (int position = 0; position < text.length(); ++position)
+    {
         char next = text[position];
 
         if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
+            if (position + 1 == text.length()) {
+                cout << position + 1;
+                return 0;
+            }
+
+            Bracket bracket(next, position);
+            opening_brackets_stack.push(bracket);
+            continue;
         }
 
         if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
+            if (opening_brackets_stack.empty()) {
+                cout << position + 1;
+                return 0;
+            }
+
+            char open = opening_brackets_stack.top().type;
+            if (open == '(' && next == ')' || open == '[' && next == ']' || open == '{' && next == '}') {
+                opening_brackets_stack.pop();
+            } else {
+                cout << position + 1;
+                return 0;
+            }
         }
     }
 
-    // Printing answer, write your code here
+    if (!opening_brackets_stack.empty()) {
+        cout << opening_brackets_stack.top().position + 1;
+    } else {
+        cout << "Success";
+    }
 
     return 0;
 }
