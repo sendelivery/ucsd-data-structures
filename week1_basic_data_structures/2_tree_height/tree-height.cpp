f7335f491d720bfeeaabafddfe0739778396aead
Seb <seb@192.168.1.114>
2022-06-26 11:24:00 +0100

    Week 1 - Tree Height

--- week1_basic_data_structures/2_tree_height/tree-height.cpp
+++ week1_basic_data_structures/2_tree_height/tree-height.cpp
@@ -1,17 +1,16 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
-#if defined(__unix__) || defined(__APPLE__)
-#include <sys/resource.h>
-#endif
+#include <queue>
+
+using namespace std;
 
 class Node;
 
 class Node {
-public:
-    int key;
+  public:
+    int label;
     Node *parent;
-    std::vector<Node *> children;
+    vector<Node *> children;
 
     Node() {
       this->parent = NULL;
@@ -23,57 +22,47 @@ public:
     }
 };
 
+int tree_height() {
+  int n, root;
+  cin >> n;
+  vector<Node> nodes(n);
 
-int main_with_large_stack_space() {
-  std::ios_base::sync_with_stdio(0);
-  int n;
-  std::cin >> n;
-
-  std::vector<Node> nodes;
-  nodes.resize(n);
-  for (int child_index = 0; child_index < n; child_index++) {
+  // populate tree, store index of root
+  for (int i = 0; i < n; ++i) {
     int parent_index;
-    std::cin >> parent_index;
-    if (parent_index >= 0)
-      nodes[child_index].setParent(&nodes[parent_index]);
-    nodes[child_index].key = child_index;
+    cin >> parent_index;
+
+    if (parent_index >= 0) 
+      nodes[i].setParent(&nodes[parent_index]);
+    else
+      root = i;
+    
+    nodes[i].label = i;
   }
 
-  // Replace this code with a faster implementation
-  int maxHeight = 0;
-  for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-    int height = 0;
-    for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-      height++;
-    maxHeight = std::max(maxHeight, height);
+  //bfs
+  queue<Node> q;
+  q.push(nodes[root]);
+  Node deepest;
+
+  while(!q.empty()) {
+    deepest = q.front();
+    q.pop();
+
+    for (int i = 0; i < deepest.children.size(); ++i)
+      q.push(*deepest.children[i]);
   }
-    
-  std::cout << maxHeight << std::endl;
-  return 0;
+
+  // traverse back up the tree
+  int height = 1;
+  while(deepest.parent != NULL) {
+    height++;
+    deepest = *deepest.parent;
+  }
+
+  return height;
 }
 
-int main (int argc, char **argv)
-{
-#if defined(__unix__) || defined(__APPLE__)
-  // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
-  struct rlimit rl;
-  int result;
-
-  result = getrlimit(RLIMIT_STACK, &rl);
-  if (result == 0)
-  {
-      if (rl.rlim_cur < kStackSize)
-      {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
-      }
-  }
-
-#endif
-  return main_with_large_stack_space();
+int main() {
+  cout << tree_height() << endl;
 }
